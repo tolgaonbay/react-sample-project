1dce2c6bddff7ab594324c37739436eb95235afe
tolgaonbay <tolgaonbay@yahoo.com>
2016-10-06 01:20:35 +0300

    removed redundant lines

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 $ = jQuery = require('jquery');
 
-test = undefined;
-var asdf = 1;
-console.log(asdfas);
-alert('ok');
-
 var App = console.log('hello world from browserify');
 
 module.exports = App;
