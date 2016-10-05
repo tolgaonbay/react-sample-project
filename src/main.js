f5d83312352c8151f6b45993f3bd2219fbb7ad8b
tolgaonbay <tolgaonbay@yahoo.com>
2016-10-06 01:13:16 +0300

    initial setup: added gulp, livereload, browserify, bootstrap, eslint component

---
+++ src/main.js
@@ -0,0 +1,10 @@
+$ = jQuery = require('jquery');
+
+test = undefined;
+var asdf = 1;
+console.log(asdfas);
+alert('ok');
+
+var App = console.log('hello world from browserify');
+
+module.exports = App;
